f3d1b8398adbcd52d7ba4af9e008e3c8f954f123
cloison <corentin.loison@ls-vr.com>
2023-05-10 10:27:15 +0200

    hooks useRefresh

---
+++ src/hooks/useRefresh.ts
@@ -0,0 +1,25 @@
+import { useState, useCallback } from "react";
+
+
+/**
+ *
+ * @name useRefresh
+ *
+ * To force a re-rendering of the component...
+ *
+ * @author filoupegase
+ *
+ */
+const useRefresh = (): [() => void, number] => {
+    const [count, setCount] = useState<number>(0);
+
+    const refresh = useCallback(() => {
+        setCount((prevCount: number) => {
+            return prevCount + 1;
+        });
+    }, []);
+
+    return [refresh, count];
+};
+
+export default useRefresh;
