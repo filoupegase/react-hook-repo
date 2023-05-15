36cbc0a310d6fb045b160b07c248e1762ca9ac11
filoupegase <loisoncorentin@yahoo.fr>
2023-05-15 12:22:16 +0200

    save

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+
+function App() {
+    return (
+        <><p>salut</p></>
+    )
+}
+
+export default App;
--- stories/useWindowScroll.story.tsx
+++ stories/useWindowScroll.story.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useWindowScroll } from "../src";
 
-
+// TODO : Coming soon
 const Demo = () => {
     //const { x, y } = useWindowScroll();
 
