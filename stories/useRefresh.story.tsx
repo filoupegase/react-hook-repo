e04dcb25a073f135f3df9676fc1b09c967c4d69d
filoupegase <loisoncorentin@yahoo.fr>
2023-05-16 11:46:16 +0200

    save

--- stories/useRefresh.story.tsx
+++ stories/useRefresh.story.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { useRefresh } from '../src';
 
 
 const Demo = () => {
+    const [refresh, count] = useRefresh();
+
+    const handleClick = (): void => {
+        refresh();
+    };
+
+    useEffect(() => {
+        console.log('passes into the useEffect because the count value has changed');
+    }, [count]);
 
     return (
         <div>
+            <button onClick={ handleClick }>Click for update data</button>
         </div>
     );
 };
